36b8482bacd60d26c13387dc449084f43490fece
Guy-Ben-Yosef <guyben@campus.technion.ac.il>
2023-07-29 16:26:33 +0300

    Solved

---
+++ CodeWars/src/BasicOperations.java
@@ -0,0 +1,11 @@
+public class BasicOperations
+{
+    public static Integer basicMath(String op, int v1, int v2)
+    {
+        if (op.equals("+")) return v1 + v2;
+        if (op.equals("-")) return v1 - v2;
+        if (op.equals("*")) return v1 * v2;
+        if (op.equals("/")) return v1 / v2;
+        return 0;
+    }
+}
